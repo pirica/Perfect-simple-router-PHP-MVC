a96d4c35e6754b930197f9336141fc0f0005e102
Warley Rodrigues <warleyhacker@hotmail.com>
2022-01-15 11:33:31 -0300

    1.2

--- Controller/sitemap.php
+++ Controller/sitemap.php
@@ -2,8 +2,8 @@
 include '../Model/conn.php';
 //example:
 //$mysqlxml = "SELECT * FROM blog ORDER BY id DESC";
-$mysqlxml = "";
-$connxml = mysqli_query($conn, "$mysqlxml");  
+// $mysqlxml = "";
+// $connxml = mysqli_query($conn, "$mysqlxml");  
 
 header("Content-type: text/xml");
 echo '<?xml version="1.0" encoding="UTF-8" ?>';
@@ -17,15 +17,15 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
     </url>
 
     <?php            
-    while($xml=mysqli_fetch_object($connxml)) {        
-        echo 
-        "<url>
-            <loc>https://warleyrodrigues.net/post?p=".$xml->id."</loc>        
-            <lastmod>".$xml->oficialdata."</lastmod>
-            <priority>1.0</priority>
-        </url>"
-        
-    ;}
-?>   
+        // while($xml=mysqli_fetch_object($connxml)) {        
+        //     echo 
+        //     "<url>
+        //         <loc>https://warleyrodrigues.net/post?p=".$xml->id."</loc>        
+        //         <lastmod>".$xml->oficialdata."</lastmod>
+        //         <priority>1.0</priority>
+        //     </url>"
+            
+        // ;}
+    ?>   
 
 </urlset>
