4d07f9a837f85af5e069d200daa25abc5c6a9557
Warley Rodrigues <warleyhacker@hotmail.com>
2022-01-15 17:21:35 -0300

    erro com public resolvido

--- Controller/ControllerBlog.php
+++ Controller/ControllerBlog.php
@@ -5,9 +5,9 @@ class ControllerBlog
     public $title = "Blog Example";
 
     public function view(){
-        require '../Public/include/header.php';
-        require '../Public/view/blog.php';
-        require '../Public/include/footer.php';
+        require './Public/include/header.php';
+        require './Public/view/blog.php';
+        require './Public/include/footer.php';
     }    
 }
 
---
+++ router.php
@@ -0,0 +1,30 @@
+<?php
+require './vendor/autoload.php';
+require './Controller/global.php';
+
+use Core\App;
+$app = new App;
+
+//Routers
+$app->view("","./Public/view/index.php");
+//example controller
+$app->view("blog","./Controller/ControllerBlog.php");
+
+
+
+
+//seo
+$app->seo("/sitemap.xml","./Controller/sitemap.php");
+$app->seo("/robots.txt","./Controller/robots.php");
+
+if($app->url == false){
+    require './Public/view/404.php';
+}
+
+if(isset($conn)){
+    mysqli_close($conn);
+}
+
+if(isset($connpdo)){         
+    $connpdo = null;    
+}
